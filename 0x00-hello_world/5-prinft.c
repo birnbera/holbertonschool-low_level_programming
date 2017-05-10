660b7fbdc9aace0b12197f33d134446f0c74f79b
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-10 21:51:53 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-prinft.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints string with printf
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
