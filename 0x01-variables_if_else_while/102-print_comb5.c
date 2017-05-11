1026198b7cd08276b380b6365194346668c0ce96
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-11 23:39:58 +0000

    102-print_comb5.c && update README.md

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations of 2 2 digit numbers separated by commas
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a = 0;
+	int b = a + 1;
+
+	while (a <= 98)
+	{
+		while (b <= 99)
+		{
+			putchar('0' + (a - a % 10) / 10);
+			putchar('0' + a % 10);
+			putchar(' ');
+			putchar('0' + (b - b % 10) / 10);
+			putchar('0' + b % 10);
+			if (b != 99 || a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
