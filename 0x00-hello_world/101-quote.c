874ec829495a889154bebbe367ade419dba194ee
birnbera <birnbera@users.noreply.github.com>
2017-05-10 18:06:22 -0700

    Add comment on fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(quote, 1, sizeof(quote), stderr);
+	fwrite(quote, 1, sizeof(quote), stderr); /* stderr is a file under linux */
 	return (1);
 }
