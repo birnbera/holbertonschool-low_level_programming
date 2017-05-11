b796ed3551f6a0d7a1f77bc98584e26970600bb0
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-11 16:34:55 +0000

    101-quote.c with unistd.h write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
-#include <stdlib.h>
+#include <unistd.h>
+
+#define quote "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
 /**
  * main - use system call to output quote to stderr
@@ -7,6 +9,6 @@
  */
 int main(void)
 {
-	system("echo \"and that piece of art is useful\\\" - Dora Korpar, 2015-10-19\" >&2");
+	write(STDERR_FILENO, quote, sizeof(quote) - 1);
 	return (1);
 }
