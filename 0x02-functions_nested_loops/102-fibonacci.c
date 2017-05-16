54b33c177d91defcfe0d77f7a4342169a74ab189
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-16 19:13:17 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	unsigned long int n1 = 1;
+	unsigned long int n2 = 2;
+	unsigned long int tmp, count;
+
+	printf("%lu", n1);
+	for (count = 1; count <= 50; count++)
+	{
+		printf(", %lu", n2);
+		tmp = n1;
+		n1 = n2;
+		n2 += tmp;
+	}
+	printf("\n");
+	return (0);
+}
