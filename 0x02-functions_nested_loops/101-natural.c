9dc1fec9753eec1e183271855f825301ef1fc838
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-16 18:51:47 +0000

    101-natural.c: fix logic

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 10
 
 /**
  * main - print sum of multiples of 3 or 5 below 1024
@@ -9,8 +10,8 @@ int main(void)
 {
 	int s = 0, i;
 
-	for (i = 1; i < 1024; i++)
-		s += (i % 3 || i % 5 ? i : 0);
+	for (i = 1; i < MAX; i++)
+		s += (!(i % 3 && i % 5) ? i : 0);
 	printf("%d\n", s);
 	return (0);
 }
