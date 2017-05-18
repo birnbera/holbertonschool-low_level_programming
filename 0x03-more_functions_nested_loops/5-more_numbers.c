662dbf88dcb9b14d6d57177d3edf0ac53d45dd18
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-18 19:47:22 +0000

    5-more_numbers.c: remove extraneous functions

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,5 @@
 #include "holberton.h"
 
-void print_numbers_no_nl(void);
-
 /**
  * more_numbers - print 0 to 14, 10 times with newline
  */
@@ -11,23 +9,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; ++i)
 	{
-		print_numbers_no_nl();
-		for (c = 10; c <= 14; ++c)
+		for (c = 0; c <= 14; ++c)
 		{
-			_putchar(c / 10 + '0');
+			if (c > 9)
+				_putchar(c / 10 + '0');
 			_putchar(c % 10 + '0');
 		}
 		_putchar('\n');
 	}
-}
-
-/**
- * print_numbers_no_nl - print 0 to 9, without newline
- */
-void print_numbers_no_nl(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; ++c)
-		_putchar(c);
 }
