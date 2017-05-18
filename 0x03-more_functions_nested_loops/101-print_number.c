9bc03c1c5a094be5bab4469873fe7ad6467f2a6f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-18 23:16:21 +0000

    101-print_number.c

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer character by character
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	int m = (n < 0 ? -1 : 1);
+	if (n * m < 10)
+	{
+		if (m == -1)
+			_putchar('-');
+		_putchar(n * m + '0');
+	}
+	else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 * m + '0');
+	}
+}
