ba06a06cfd0ddcaa93cb0a8198bec091ea14a7e4
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-25 17:48:49 +0000

    2-strncpy.c

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+/**
+ * _strncpy - copy at most n characters from `src' into `dest'
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (n && (dest[i] = src[i]))
+		++i, --n;
+	while (n--)
+		dest[++i] = '\0';
+	return (dest);
+}
