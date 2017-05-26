99a2bd5e314343e9bfb1a5fc6de37801f4db6e97
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-05-26 23:46:09 +0000

    102-infinite_add.c: fix length

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,4 +1,35 @@
 /**
+ * get_hi - get highest index character array `s'
+ * @s: character array to check
+ *
+ * Return: highest index
+ */
+int get_hi(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; ++i)
+		;
+	return (i);
+}
+
+/**
+ * rev_str - reverse string `s' in place
+ * @s: string to reverse
+ */
+void rev_str(char *s, int hi)
+{
+	int lo, tmp;
+
+	for (lo = 0; lo < hi; ++lo, --hi)
+	{
+		tmp = s[lo];
+		s[lo] = s[hi];
+		s[hi] = tmp;
+	}
+}
+
+/**
  * infinite_add - add `n1' and `n2' as though they were numbers
  * @n1: character string representing `n1'
  * @n2: character string representing `n2'
@@ -13,45 +44,29 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, tmp, carry, len1, len2;
+	int i, res, carry, len1, len2;
 
-/* Get length of `n1' */
-	for (i = 0; n1[i]; ++i)
-		;
-	len1 = i;
-/* Get length of `n2' */
-	for (i = 0; n2[i]; ++i)
-		;
-	len2 = i;
-/* While there's still room in `r' accumulate from `n1' and `n2' */
+	len1 = get_hi(n1);
+	len2 = get_hi(n2);
 	for (carry = 0, i = 0; i < size_r - 1; ++i)
 	{
-		k = carry;
-		if (len1)
-			k += n1[--len1] - '0';
-		if (len2)
-			k += n2[--len2] - '0';
-		if (!len1 && !len2 && !k)
+		res = carry;
+		if (len1 != 0)
+			res += n1[--len1] - '0';
+		if (len2 != 0)
+			res += n2[--len2] - '0';
+		if (len1 == 0 && len2 == 0 && res == 0)
 		{
 			r[i] = '\0';
 			break;
 		}
-		carry = k / 10;
-       		r[i] = k % 10 + '0';
-       		r[i + 1] = '\0';
-       	}
-/* If at end of `r' and carry not `0' or not at end of `n1' or `n2', */
-/* `r' is too small                                                  */
+		carry = res / 10;
+		r[i] = res % 10 + '0';
+		r[i + 1] = '\0';
+	}
 	if (carry || len1 || len2)
 		return (0x0);
-	for (i = 0; r[i]; ++i)
-		;
-	--i;
-	for (j = 0; j < i; ++j, --i)
-	{
-		tmp = r[j];
-		r[j] = r[i];
-		r[i] = tmp;
-	}
+	i = get_hi(r);
+	rev_str(r, --i);
 	return (r);
 }
