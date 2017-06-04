43dd70e10cb7b12c4b876abbec277facf67808b1
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-04 20:25:28 +0000

    0-whatismyname.c: change __attribute__ to void

--- 0x09-argc_argv/0-whatismyname.c
+++ 0x09-argc_argv/0-whatismyname.c
@@ -7,8 +7,9 @@
  *
  * Return: 0 always
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
