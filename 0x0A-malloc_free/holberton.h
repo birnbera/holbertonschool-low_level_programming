7b9e5b283cad4394b798cf9b0f50cf5acdc931fa
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-06 21:59:23 +0000

    Guard against multiple includes

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -1,3 +1,5 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 void _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -5,3 +7,4 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+#endif /* _HOLBERTON_H_ */
