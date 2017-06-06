4ec611b005ed40fe6ba8aa9f3472ff76bc284c44
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-06 23:04:03 +0000

    2-str_concat.c: fix test for NULL pointer

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * str_concat - concatenate `s1' and `s2' and return pointer to new string
@@ -9,7 +10,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, k;
 	char *p;
 
 	for (i = 0; s1 && s1[i]; ++i)
@@ -19,10 +20,11 @@ char *str_concat(char *s1, char *s2)
 	p = malloc((i + j + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	*(p + i + j) = '\0';
-	while (*s1)
-		*p++ = *s1++;
-	while (*s2)
-		*p++ = *s2++;
-	return (p - i - j);
+	k = 0;
+	for (i = 0; s1 && s1[i]; ++i, ++k)
+		p[k] = s1[i];
+	for (j = 0; s2 && s2[j]; ++j, ++k)
+		p[k] = s2[i];
+	p[k] = '\0';
+	return (p);
 }
