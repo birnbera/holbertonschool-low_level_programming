359ddc980aef3044b055c1582c2af9afdbd77db6
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-06 22:25:14 +0000

    0-create_array.c: change NULL to 0 for testing

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *arr = malloc(size * sizeof(char));
 	unsigned int i;
 
-	if (arr == NULL)
-		return (NULL);
+	if (arr == 0)
+		return (0);
 
 	for (i = 0; i < size; ++i)
 		arr[i] = c;
