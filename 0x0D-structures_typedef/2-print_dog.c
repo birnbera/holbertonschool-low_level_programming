17b1174c180af618767f879a0454e42f3061989f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-12 23:52:56 +0000

    2-print_dog.c: fix typos and logic for age

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,9 +10,6 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
-	printf("Owner: %s\n", d->name ? d->owner : "(nil)");
+	printf("Age: %f\n", d->age);
+       	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
