a22b50cd6496e2ada435c7ea1d54a400dcb3edbb
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-12 23:54:13 +0000

    2-print_dog.c: fix style

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,5 +11,5 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-       	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
