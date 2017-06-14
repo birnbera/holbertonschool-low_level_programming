82d4f7cbe3bff3dbbbbfec69acbab9ecef5dfe63
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-14 19:39:55 +0000

    1-array_iterator.c: check for nonexistent array

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL || size == 0)
 		exit(EXIT_FAILURE);
 	for (i = 0; i < size; ++i)
 		action(array[i]);
