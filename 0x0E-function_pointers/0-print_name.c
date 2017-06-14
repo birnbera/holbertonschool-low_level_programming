70d98e321fc5fda7fa2fbcd333f51a39884050b2
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-14 19:36:20 +0000

    0-print_name.c: check for NULL pointer

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - print `name' using function `f'
  * @name: string to print
@@ -5,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		exit(EXIT_FAILURE);
 	f(name);
 }
