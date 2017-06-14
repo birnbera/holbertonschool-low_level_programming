bd61c2282c1fe77824d33a38950eaefb9624d7ca
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-14 19:51:25 +0000

    2-int_index.c: exit -1

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		exit(EXIT_FAILURE);
+		exit(-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; ++i)
