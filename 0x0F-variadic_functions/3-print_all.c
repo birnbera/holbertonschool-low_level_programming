ee79582e73f577bbda4ce0be03d1df2b19d42de2
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-16 18:27:11 +0000

    Exercises 0-3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *s;
+	int i = 0;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		switch (format[i++])
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			s = va_arg(ap, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			break;
+		default:
+			continue;
+		}
+		if (format[i])
+			printf(", ");
+	}
+	putchar('\n');
+}
