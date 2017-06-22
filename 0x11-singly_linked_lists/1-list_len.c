a382e1a2b0f79fa038a43bde6a068ef79705ca8d
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-06-22 20:35:48 +0000

    1-list_len.c: add documentation

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - count number of elements of list `h'
+ * @h: pointer to head of list
+ *
+ * Return: size of list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t size = 0;
