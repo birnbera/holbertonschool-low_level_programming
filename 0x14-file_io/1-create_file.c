2be1952bba890e4ca005edf57777d5de67a58d97
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-07-04 00:19:44 +0000

    1-create_file.c + holberton.h: move inludes to header

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,8 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "holberton.h"
 
 /**
  * create_file - Create `filename' with content from `text_content'
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,6 +1,13 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
