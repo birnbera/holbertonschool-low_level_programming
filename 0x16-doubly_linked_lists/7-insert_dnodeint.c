cfe497454e1f27c22b37522aab607663f086e276
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-05 22:22:35 +0000

    7-insert_dnodeint.c: fix for idx==0

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,6 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 		new->prev = NULL;
 		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
