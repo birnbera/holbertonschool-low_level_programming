9b801a530cc796071b55f67c33c23c85f52460db
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-08 18:49:10 +0000

    103-keygen.c: fix for betty

--- 0x16-doubly_linked_lists/103-keygen.c
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -2,89 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t f2(char *);
-size_t f3(char *);
-int f4(char *);
-size_t f5(char *);
-int f6(char);
-
+/**
+ * main - takes commandline arg of username and produces corresponding key
+ * @argc: number of commandline args
+ * @argv: array of commandline args
+ *
+ * Return: 0 on success, 1 otherwise
+ */
 int main(int argc, char *argv[])
 {
-	char *uname, pw[7];
-	char *hash = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+\
-4mjW6fxqZeF3Qa1rPhdKIouk";
+	size_t i, j;
+	int rnd;
+	char *h, *uname, pw[7], a;
+
+	h = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	if (argc != 2)
-		exit (1);
+		exit(1);
 	uname = argv[1];
-	pw[0] = hash[(strlen(uname) ^ 0x3b) & 0x3f];
-	pw[1] = hash[f2(uname)];
-	pw[2] = hash[f3(uname)];
-	pw[3] = hash[f4(uname)];
-	pw[4] = hash[f5(uname)];
-	pw[5] = hash[f6(uname[0])];
+	j = (strlen(uname) ^ 0x3b) & 0x3f;
+	pw[0] = h[j];
+
+	for (j = i = 0; uname[i]; i++)
+		j += uname[i];
+	pw[1] = h[(j ^ 0x4f) & 0x3f];
+
+	for (j = 1, i = 0; uname[i]; i++)
+		j *= uname[i];
+	pw[2] = h[(j ^ 0x55) & 0x3f];
+
+	for (a = uname[0], i = 0; i < strlen(uname); i++)
+		if (uname[i] > a)
+			a = uname[i];
+	srand(a ^ 0xe);
+	j = rand();
+	pw[3] = h[j & 0x3f];
+
+	for (j = i = 0; uname[i]; i++)
+		j += (uname[i] * uname[i]);
+	pw[4] = h[(j ^ 0xef) & 0x3f];
+
+	for (a = uname[0]; a > 0; a--)
+		rnd = rand();
+	pw[5] = h[(rnd ^ 0xe5) & 0x3f];
+
 	pw[6] = '\0';
 	printf("%s", pw);
 	return (0);
-}
-
-size_t f2(char *uname)
-{
-	size_t i = 0;
-
-	while (*uname)
-	{
-		i += *uname;
-		uname++;
-	}
-	return ((i ^ 0x4f) & 0x3f);
-}
-
-size_t f3(char *uname)
-{
-	size_t i = 1;
-
-	while (*uname)
-	{
-		i *= *uname;
-		uname++;
-	}
-	return ((i ^ 0x55) & 0x3f);
-}
-
-int f4(char *uname)
-{
-	char a;
-	size_t i;
-
-	for (a = uname[0], i = 0; i < strlen(uname); i++)
-	{
-		if (uname[i] > a)
-			a = uname[i];
-	}
-	srand(a ^ 0xe);
-	return (rand() & 0x3f);
-}
-
-size_t f5(char *uname)
-{
-	size_t i = 0;
-
-	while (*uname)
-	{
-		i += *uname * *uname;
-		uname++;
-	}
-	return ((i ^ 0xef) & 0x3f);
-}
-
-int f6(char first)
-{
-	int rnd;
-
-	while (first > 0)
-	{
-		rnd = rand();
-		first--;
-	}
-	return ((rnd ^ 0xe5) & 0x3f);
 }
