3961b1a04feec09f04145d5d727b9a47c7536beb
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-23 23:48:32 +0000

    0-8 binary trees

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node as left-child of `parent`
+ * @parent: parent of new node
+ * @value: integer value to store in new node
+ *
+ * Return: pointer to new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
+}
---
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - pre-order traverse of binary tree
+ * @tree: starting point of traversal
+ * @func: function to call on the value at each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
