2014078acfc33b8e8acc037f4d830b4ffa608b71
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-25 21:30:02 +0000

    If parent is null do not create new node

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -9,15 +9,16 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
+	new_node->left = parent->left;
 	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
 		parent->left->parent = new_node;
-	}
 	parent->left = new_node;
 	return (new_node);
 }
--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -9,15 +9,16 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
+	new_node->right = parent->right;
 	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
 		parent->right->parent = new_node;
-	}
 	parent->right = new_node;
 	return (new_node);
 }
