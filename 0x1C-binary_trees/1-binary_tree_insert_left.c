f9485af19f50e64939c86dc387eb7dc854eba2b6
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-25 21:08:22 +0000

    Fix style

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
 		return (NULL);
 	if (parent->left != NULL)
