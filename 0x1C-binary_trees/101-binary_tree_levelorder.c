90f307954cb33a2a86a5d38270d782108099910f
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-26 21:11:05 +0000

    Check for null function pointer

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -61,7 +61,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_t *node;
 
 	head = tail = NULL;
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 	push(&head, &tail, tree);
 	while (head)
