d74636450bb7060729cd20bff6639f4dfdbbd0b3
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-26 19:54:32 +0000

    Update height function

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,8 +10,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
-	return (1 + max(binary_tree_height(tree->left),
-			binary_tree_height(tree->right)));
+	if (tree->left == NULL && tree->right == NULL)
+		return (tree->parent != NULL);
+	return (1 + max(height(tree->left),
+			height(tree->right)));
 }
