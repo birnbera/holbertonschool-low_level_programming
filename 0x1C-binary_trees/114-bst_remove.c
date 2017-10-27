aa511ca8a14d5603e793899e8ff8d3cb88e76359
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-10-27 22:24:52 +0000

    Will not pass betty

---
+++ 0x1C-binary_trees/114-bst_remove.c
@@ -0,0 +1,97 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - search for value in binary search tree
+ * @tree: root of tree to search
+ * @value: value to search for
+ *
+ * Return: pointer to node containing value, or NULL if not found
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (value == tree->n)
+		return ((bst_t *) tree);
+	else if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
+
+/**
+ * bst_remove - remove node from binary search tree with value equal to `value`
+ * @root: root of tree from which to remove node
+ * @value: value of node to remove
+ *
+ * Return: pointer to new root of tree
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *child, *to_remove = bst_search(root, value);
+
+	if (to_remove == NULL)
+		return (root);
+	if (to_remove->left == NULL && to_remove->right == NULL)
+	{
+		if (to_remove->parent == NULL)
+		{
+			free(to_remove);
+			return (NULL);
+		}
+		if (to_remove == to_remove->parent->left)
+		{
+			to_remove->parent->left = NULL;
+			free(to_remove);
+			return (root);
+		}
+		to_remove->parent->right = NULL;
+		free(to_remove);
+		return (root);
+	}
+	else if ((to_remove->left == NULL) ^ (to_remove->right == NULL))
+	{
+		child = (to_remove->left == NULL ?
+			 to_remove->right : to_remove->left);
+		if (to_remove->parent == NULL)
+		{
+			child->parent = NULL;
+			free(to_remove);
+			return (child);
+		}
+		if (to_remove == to_remove->parent->left)
+		{
+			child->parent = to_remove->parent;
+			to_remove->parent->left = child;
+			free(to_remove);
+			return (root);
+		}
+		child->parent = to_remove->parent;
+		to_remove->parent->right = child;
+		free(to_remove);
+		return (root);
+	}
+	else
+	{
+		child = to_remove->right;
+		while (child->left != NULL)
+			child = child->left;
+		to_remove->right->parent = child;
+		to_remove->left->parent = child;
+		if (to_remove->parent != NULL)
+		{
+			if (to_remove->parent->left == to_remove)
+				to_remove->parent->left = child;
+			else
+				to_remove->parent->right = child;
+		}
+		child->parent->left = child->right;
+		if (child->right != NULL)
+			child->right->parent = child->parent;
+		child->left = to_remove->left;
+		child->right = to_remove->right;
+		child->parent = to_remove->parent;
+		free(to_remove);
+		return (child->parent ? root : child);
+	}
+}
